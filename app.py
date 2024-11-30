d5370a64fb45d5d03f45e777da581dc19bc435d1
SandyDRawat <sr9644774485@gmail.com>
2024-11-30 20:20:57 +0530

    Add comments to app.py script file and update the save location of the outputs csv files to saved_data folder

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import streamlit as st
 import pandas as pd
 from data_intake.data_intake import get_data
 
-
+# Initial message to indicate the app is running
 st.write("Streamlit app is running")
 
+# Create an input widget for the user to specify the number of days
+no_of_days = st.number_input(
+    'Enter the number of days', 
+    min_value=1,   # Minimum allowed value
+    max_value=10,  # Maximum allowed value
+    value=1        # Default value
+)
 
-no_of_days = st.number_input('Enter the number of days', min_value=1, max_value=10, value=1)
-
+# Call the `get_data` function from the custom `data_intake` module
+# Pass the user-specified number of days as a parameter
 data = get_data(no_of_days)
 
-#st.write(data)
+# Display the retrieved data in a scrollable and interactive table
+# The `st.dataframe` function renders a DataFrame in Streamlit
 st.dataframe(data)
-
-
